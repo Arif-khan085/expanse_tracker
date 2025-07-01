493fa363d28bcfd22b4c394910a9f7bb9237b1e6
Arif khan <arif0852147@gmail.com>
2025-07-01 12:48:00 +0500

    First Ui

--- lib/res/assets/images.dart
+++ lib/res/assets/images.dart
@@ -1,7 +1,8 @@
 
 
 
-class AssetImage{
-
-  static const String splashScreen =  'res/assets/app_logo.png';
+// app_images.dart
+class AppImages {
+  static const String splashScreen = 'res/assets/app_logo.png';
+  static const String firstScreen = 'res/assets/expense.png';
 }
--- lib/view/login/login_view.dart
+++ lib/view/login/login_view.dart
@@ -1,7 +1,11 @@
 
 
+import 'package:expense_tracker/res/colors/app_colors.dart';
+import 'package:expense_tracker/res/components/round_button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../../res/assets/images.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -14,7 +18,22 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Login'),),
+      backgroundColor:AppColors.backgroundColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image(image: AssetImage(AppImages.firstScreen)),
+            Text('Expense Tracker',style: TextStyle(color: AppColors.whiteColor,fontSize: 25),),
+            Text('Welcome back',style: TextStyle(color: AppColors.whiteColor,fontSize: 30),),
+            RoundButtontwo(
+
+                title: 'login', onPress: (){}),
+            RoundButton(title: 'login', onPress: (){})
+          ],
+        ),
+      ),
     );
   }
 }
